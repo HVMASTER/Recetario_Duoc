5488c055eac1e53a111efc2d4084ea440d44c3f9
oscar <oscarolivare1@gmail.com>
2022-11-12 22:07:18 -0300

    implementacion a conexion base de datos

--- src/Bd/Conexion.java
+++ src/Bd/Conexion.java
@@ -4,11 +4,31 @@
  * and open the template in the editor.
  */
 package Bd;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  *
  * @author carlosabarca
  */
 public class Conexion {
+
+    public Connection obtenerConexion() {
+        
+        Connection conexion = null;
+        
+        try {
+            
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/recetario", "root", "");
+            
+            System.out.println("Conexion exitosa !");
+            
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Error de conexion: " + e.getMessage());
+        }
+        return conexion;
+    }
     
 }
