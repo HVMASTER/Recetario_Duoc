4c534941478efcdccb68441a5bccdb9772395825
Hernán Vallejos <hernanvallejos94@gmail.com>
2022-11-13 00:10:22 -0300

    Add the new package "Controlador" more "principal" and "Registro de usuario" class

---
+++ src/Controlador/Principal.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controlador;
+
+import Modelo.CategoriaCoctel;
+import Modelo.CategoriaReceta;
+import Modelo.Coctel;
+import Modelo.Receta;
+import Modelo.Usuario;
+
+/**
+ *
+ * @author Hernán
+ */
+public class Principal {
+    
+    public static void main(String[] args) {
+        
+    Usuario usuario = new Usuario();
+    Receta receta = new Receta();
+    Coctel coctel = new Coctel();
+    CategoriaReceta categoriaReceta = new CategoriaReceta();
+    CategoriaCoctel categoriaCoctel = new CategoriaCoctel();
+    
+    
+    
+        
+    }
+    
+    
+}
---
+++ src/Controlador/RegistroUsuario.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controlador;
+
+import Bd.Conexion;
+import Modelo.Usuario;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Hernán
+ */
+public class RegistroUsuario {
+    
+    public boolean AgregarNuevoUsuario(Usuario usuario){
+        
+      try{
+          
+      
+        Conexion conexion = new Conexion();
+        Connection connx = conexion.obtenerConexion();
+        
+        String queryNombreUsuario = "INSERT INTO usuario(nombreUsuario) VALUES = (?)";
+        PreparedStatement pstNomUser = connx.prepareStatement(queryNombreUsuario);
+               
+        String queryCorreoElectronico = "INSERT INTO usuario(correoElectronico) VALUES = (?)";
+        PreparedStatement pstCorreo = connx.prepareStatement(queryCorreoElectronico);
+        
+        String queryContraseña = "INSERT INTO usuario(contraseña) VALUES = (?)";
+        PreparedStatement pstContraseña = connx.prepareStatement(queryContraseña);
+        
+        pstNomUser.executeUpdate();
+        pstNomUser.close();
+        
+        pstCorreo.executeUpdate();
+        pstCorreo.close();
+        
+        pstContraseña.executeUpdate();
+        pstContraseña.close();
+        
+        connx.close();
+        System.out.println("Usuario agregado Correctamente");
+        return true;
+        
+        
+      }catch(SQLException exception){
+          
+          System.out.println("No se pudo agregar los cambios a la Base de Datos SQL");
+          return false;
+          
+          
+          
+      }
+            
+        
+
+        
+        
+        
+    }
+    
+}
