c090b42cf54203b5e7338fa3af4ac60b8d7293f9
Hernán Vallejos <hernanvallejos94@gmail.com>
2022-11-13 02:10:46 -0300

    Se agrega metodo para mostrar recetas en la base de datos

--- src/Controlador/RegistroUsuario.java
+++ src/Controlador/RegistroUsuario.java
@@ -1,10 +1,14 @@
 package Controlador;
 
 import Bd.Conexion;
+import Modelo.Receta;
 import Modelo.Usuario;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.ResultSet;
 /**
  *
  * @author Hernán
@@ -44,7 +48,41 @@ public class RegistroUsuario {
           System.out.println("No se pudo agregar los cambios a la Base de Datos SQL");
           return false; 
       }
+      
+    }
     
+    public List<Receta> MostrarReceta(){
+        
+        List<Receta> receta = new ArrayList<>();
+        
+        try{
+            Conexion conexion = new Conexion();
+            Connection connx = conexion.obtenerConexion();
+
+            String queryReceta = "SELECT nombreReceta, ingredienteReceta, descripcion FROM receta ORDER BY nombreReceta";
+            PreparedStatement pstReceta = connx.prepareStatement(queryReceta);
+
+            ResultSet res = pstReceta.executeQuery();
+
+            while(res.next()){
+                Receta recetaC = new Receta();
+                recetaC.setNombreReceta(res.getString("nombreReceta"));
+                recetaC.setIngredienteReceta(res.getString("ingredienteReceta"));
+                recetaC.setDescripcion(res.getString("descripcion"));
+
+                receta.add(recetaC);    
+            }
+
+            res.close();
+            pstReceta.close();
+            connx.close();
+            
+        }catch(SQLException e){
+            
+            System.out.println("No se pudieron tomar los datos desde la Base de Datos SQL");   
+        }
+        
+        return receta;
     }
     
 }
